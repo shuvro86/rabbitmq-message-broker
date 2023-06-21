f3b193033c1dbc1e808c82bb1a2ba99ae846e2ff
shuvro86 <37668209+shuvro86@users.noreply.github.com>
2023-06-22 01:46:02 +0600

    Update publisher.go

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -8,7 +8,7 @@ import (
         "github.com/streadway/amqp"
         "os"
 )
-	var rabbit_host = os.Getenv("RABBIT_HOST")
+var rabbit_host = os.Getenv("RABBIT_HOST")
 var rabbit_port = os.Getenv("RABBIT_PORT")
 var rabbit_user = os.Getenv("RABBIT_USERNAME")
 var rabbit_password = os.Getenv("RABBIT_PASSWORD")
